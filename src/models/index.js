247f59607e302e5cb53a429c7d606538b942fe00
Cristian Luna <cristiansebastian.luna@estudiantes.unahur.edu.ar>
2024-10-19 21:16:37 -0300

    relaciones n:n

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,13 @@ const Producto = require('./Producto')
 const Fabricante = require('./Fabricante')
 const Componente = require('./Componente')
 
+
+Producto.belongsToMany(Fabricante, {through:"Producto_Fabricante" })
+Fabricante.belongsToMany(Producto, {through:"Producto_Fabricante" })
+
+Producto.belongsToMany(Componente, {through:"Producto_Componente" })
+Componente.belongsToMany(Producto, {through:"Producto_Componente" })
+
 module.exports = {
     Producto,
     Fabricante,
