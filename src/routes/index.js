249cfe4551514a85d4aa11418e261778d9a1e355
Cristian Luna <cristiansebastian.luna@estudiantes.unahur.edu.ar>
2024-10-20 13:00:36 -0300

    en routes-index se agrego fabricante y componente con sus controladores

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require('express')
 const productoRoutes = require('./producto.routes')
+const fabricanteController = require('./fabricante.routes')
+const componenteController = require('./componente.routes')
 const router = Router()
 
 router.use('/productos', productoRoutes)
-
+router.use('/fabricantes', fabricanteController)
+router.use('/componentes', componenteController)
 module.exports = router
