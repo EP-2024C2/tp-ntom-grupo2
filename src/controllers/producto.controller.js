23a74ab4bbc2ad4a4a9317360dd8308d0c4235ca
Cristian Luna <cristiansebastian.luna@estudiantes.unahur.edu.ar>
2024-10-20 01:02:52 -0300

    en producto se  agregaron constroladores y rutas

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -1,12 +1,6 @@
 const { Producto } = require('../models')
 const productosController= {}
 
-const getProductos = async (req,res) => {
-    const productos = await Producto.findAll()
-    res.json(productos)
-}
-productosController.getProductos = getProductos
-
 const addProducto = async (req,res) => {
     const{nombre, descripcion,precio,pathImg} = req.body
 
@@ -16,4 +10,43 @@ const addProducto = async (req,res) => {
     res.json(producto)
 }
 productosController.addProducto= addProducto
+
+const getProductos = async (req,res) => {
+    const productos = await Producto.findAll()
+    res.json(productos)
+}
+productosController.getProductos = getProductos
+
+
+
+const getProductoById = async (req,res) => {
+    const id = req.params.id
+    const producto = await Producto.findOne({
+        where: {id},
+        attributes: ['nombre','descripcion','precio']
+    })
+    res.json(producto)
+}
+productosController.getProductoById = getProductoById
+
+const updateProducto = async (req,res) =>{
+    const{nombre,descripcion,precio,pathImg} = req.body
+    const id = req.params.id
+    const productoAActualizar = await Producto.update({
+        nombre:nombre,
+        descripcion:descripcion,
+        pathImg:pathImg,
+        precio:precio,
+    },{where: {id}})
+    res.status(200).json(productoAActualizar)//VER XQ DEVUELVE EL ID
+}
+productosController.updateProducto= updateProducto
+
+const deleteProductoById = async (req,res) => {
+    const id = req.params.id
+    const productoEliminado = await Producto.destroy({where: {id}})
+    res.status(204).json({mensaje:  `el producto ${productoEliminado} fue eliminado`})
+}
+productosController.deleteProductoById = deleteProductoById
+
 module.exports = productosController
--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -4,7 +4,13 @@ const productoController = require('../controllers/producto.controller')
 const routes = Router()
 
 routes.get('/' ,productoController.getProductos )
+routes.get('/:id',productoController.getProductoById)
 routes.post('/' ,productoController.addProducto )
+routes.put('/:id',productoController.updateProducto)
+routes.delete('/:id',productoController.deleteProductoById)
+
+
+
 
 
 module.exports = routes
