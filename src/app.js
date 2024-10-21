2ed29f9765bf227939d4275dd59b24ec19e58006
MarceloTorres88 <russo1988torres@gmail.com>
2024-10-21 13:44:21 -0300

    Configuración de variables

--- src/app.js
+++ src/app.js
@@ -1,35 +1,28 @@
 require('dotenv').config();
-const express = require('express')
-const routes  = require('./routes/index')
-const sequelize = require('../config/database')
-const initialProductoFabricanteComponente= require('./seeders/initialSeeders')
-const app = express()
+const express = require('express');
+const routes = require('./routes/index');
+const sequelize = require('../config/database');
+const initialProductoFabricanteComponente = require('./seeders/initialSeeders');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(routes);
 
-app.use(routes)
-
-async function startDatabase(){
+async function startDatabase() {
     try {
-        await sequelize.sync({force: true})
-        console.log('Base de datos sincronizada')
+        await sequelize.sync({ force: true });
+        console.log('Base de datos sincronizada');
 
-        await initialProductoFabricanteComponente()
-        console.log('Datos de inicialización cargados correctamente')
+        await initialProductoFabricanteComponente();
+        console.log('Datos de inicialización cargados correctamente');
     } catch (error) {
-        console.log('Error al sicronizar o inicializar los datos')
+        console.log('Error al sincronizar o inicializar los datos', error);
     }
-} 
+}
 
-startDatabase()
+startDatabase();
 
-/*
-sequelize.sync()
-    .then(() => console.log('Base de datos sincronizada'))
-    .catch(err => console.log('Error al sincronizar la base de datos', err));
-*/
-
-const port = process.env.port || 3002;
-app.listen(port, ()=>{
-    console.log(`Ejecutando servidor en puerto ${port}`)
-})
+const port = process.env.PORT || 3008;
+app.listen(port, () => {
+    console.log(`Ejecutando servidor en puerto ${port}`);
+});
